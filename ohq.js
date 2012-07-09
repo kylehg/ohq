72ede3c3f2274bdd925f7360db5f24743603de2d
Kyle Hardgrave <kyle@kylehardgrave.com>
2012-07-09 02:51:31 -0400

    Get basic Jade templates working

--- ohq.js
+++ ohq.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var jade = require('jade');
 
 var app = express.createServer(express.logger());
 
+
+/* Config */
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
+
+  app.set('views', __dirname + '/views');
+});
+
+app.configure('development', function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+
+// App
 app.get('/', function(req, res) {
-  res.send('Hello, world!');
+  res.render('index.jade', {
+    pageTitle: 'Office Hours Queue',
+    youAreUsingJade: req.param('jade', '0') == '1'
+  });
 });
 
 var port = process.env.PORT || 5000;
